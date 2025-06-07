68f4587403825f1a793d2b0d597906bd7452e224
driesderyck <dries.deryck@hotmail.com>
2025-06-07 12:48:19 +0200

    removing DoubleML directory

--- DoubleML/src soil/util.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def calculate_mean_std(dataframe: pd.DataFrame):
-    return np.nanmean(dataframe), np.nanstd(dataframe)
-
-def normalize_df(dataframe: pd.DataFrame):
-    return (dataframe-np.nanmean(dataframe))/np.nanstd(dataframe)
