0d091b25c35b82b7546c26be3b568d5ae177ed17
driesderyck <dries.deryck@hotmail.com>
2025-06-08 21:56:18 +0200

    updating sim-evaluation

--- src/util.py
+++ src/util.py
@@ -1,5 +1,8 @@
+import os
 import numpy as np
 import pandas as pd
+from src.evaluate import _edge_roc_curve
+from pathlib import Path
 
 
 def calculate_mean_std(dataframe: pd.DataFrame):
@@ -81,3 +84,18 @@ def read_ini_file(config, file='settings.ini', automl=False):
                 learner_settings[setting] = float(learner_settings[setting])
 
     return seed, learner, learner_settings
+
+def _roc_curve(name, ranks, edges, k_max=100, axis=1):
+    # if _edges_roc_curve has been calculated before, load from files
+    if os.path.exists(os.path.join('./results/roc/', name)):
+        res = [pd.read_csv(os.path.join('./results/roc/', name, f'{name}_0.tsv'), sep='\t', index_col=0),
+               pd.read_csv(os.path.join('./results/roc/', name, f'{name}_1.tsv'), sep='\t', index_col=0),
+               pd.read_csv(os.path.join('./results/roc/', name, f'{name}_2.tsv'), sep='\t', index_col=0),
+               ]
+    # else do calculation (takes some time)
+    else:
+        res = _edge_roc_curve(ranks, edges, k_max, axis=axis)
+        Path(os.path.join('./results/roc/', name)).mkdir(parents=True, exist_ok=False)
+        for i in range(len(res)):
+            res[i].to_csv(os.path.join('./results/roc/', name, f'{name}_{str(i)}.tsv'), sep='\t', index=True)
+    return res
